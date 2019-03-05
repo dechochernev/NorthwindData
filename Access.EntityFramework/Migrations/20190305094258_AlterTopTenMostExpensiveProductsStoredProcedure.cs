c105552f3f41e9facfd85b5f733252819ec70b1c
Decho Chernev <d.chernev@outlook.com>
2019-03-05 11:02:06 +0100

    Initial migration and stored procedure migration

---
+++ Access.EntityFramework/Migrations/20190305094258_AlterTopTenMostExpensiveProductsStoredProcedure.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Access.EntityFramework.Migrations
+{
+    public partial class AlterTopTenMostExpensiveProductsStoredProcedure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER procedure [dbo].[Ten Most Expensive Products] AS " +
+                "SET ROWCOUNT 10 " +
+                "SELECT Products.ProductName, Products.UnitPrice " +
+                "FROM Products " +
+                "ORDER BY Products.UnitPrice DESC");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER procedure [dbo].[Ten Most Expensive Products] AS " +
+                "SET ROWCOUNT 10 " +
+                "SELECT Products.ProductName AS TenMostExpensiveProducts, Products.UnitPrice " +
+                "FROM Products " +
+                "ORDER BY Products.UnitPrice DESC");
+        }
+    }
+}
